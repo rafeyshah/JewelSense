c6475037efe386b7842218ed5a0ebc653e2e16e6
Abdul Rafey <dev.abdulrafey@gmail.com>
2025-04-22 02:45:50 +0200

    Text to Image Search

--- clip_search/search_similar.py
+++ clip_search/search_similar.py
@@ -10,6 +10,7 @@ from PIL import Image
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 MODEL, PREPROCESS = clip.load("ViT-B/32", device=DEVICE)
 
+
 def display_results(query_path, results, dataset_folder):
     plt.figure(figsize=(15, 4))
     plt.subplot(1, len(results)+1, 1)
@@ -27,6 +28,34 @@ def display_results(query_path, results, dataset_folder):
 
     plt.tight_layout()
     plt.show()
+
+
+def display_results(query, results, dataset_folder, query_type="image"):
+    plt.figure(figsize=(15, 4))
+
+    if query_type == "image":
+        plt.subplot(1, len(results)+1, 1)
+        plt.imshow(Image.open(query))
+        plt.title("Query")
+        plt.axis('off')
+    else:
+        plt.subplot(1, len(results)+1, 1)
+        plt.text(0.5, 0.5, f'"{query}"', wrap=True, fontsize=14,
+                 ha='center', va='center')
+        plt.axis('off')
+        plt.title("Prompt")
+
+    for i, filename in enumerate(results):
+        path = os.path.join(dataset_folder, filename)
+        img = Image.open(path)
+        plt.subplot(1, len(results)+1, i+2)
+        plt.imshow(img)
+        plt.title(f"Match {i+1}")
+        plt.axis('off')
+
+    plt.tight_layout()
+    plt.show()
+
 
 def search_similar(query_path, index_path="clip_search/index", dataset_path="data/test/images", top_k=5):
     # Load query image
@@ -51,6 +80,24 @@ def search_similar(query_path, index_path="clip_search/index", dataset_path="dat
     display_results(query_path, results, dataset_path)
 
 
+def search_by_text(prompt, index_path="clip_search/index", dataset_path="data/test/images", top_k=5):
+    with torch.no_grad():
+        text_tokens = clip.tokenize([prompt]).to(DEVICE)
+        text_embedding = MODEL.encode_text(
+            text_tokens).cpu().numpy().astype('float32')
+
+    index = faiss.read_index(f"{index_path}/clip.index")
+    with open(f"{index_path}/filenames.pkl", "rb") as f:
+        filenames = pickle.load(f)
+
+    distances, indices = index.search(text_embedding, top_k)
+    results = [filenames[i] for i in indices[0]]
+
+    print(f"\n[✓] Top {top_k} results for prompt: \"{prompt}\"")
+    display_results(prompt, results, dataset_path, query_type="text")
+
+
 if __name__ == "__main__":
     # Example
-    search_similar("query.jpg")
+    # search_similar("query.jpg")
+    search_by_text("Necklace and rings ")  # Text-based
